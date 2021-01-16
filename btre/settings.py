76af978b0ca27b96db0e861c27c8d7aaebcce386
charly2009 <marichal06@hotmail.com>
2021-01-16 13:56:51 +0100

    'settings'

--- btre/settings.py
+++ btre/settings.py
@@ -144,14 +144,7 @@ MESSAGE_TAGS = {
 
 }
 
-# Optional SMTP authentication information for EMAIL_HOST.
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'dossosoualiho@gmail.com'
-EMAIL_HOST_PASSWORD = 'ndsojfslbxuzlohc'
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'default from email'
+
 
 try:
     from .local_settings import*
